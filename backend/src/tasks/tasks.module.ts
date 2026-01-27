a4ac795190c3a77d31f350b85ce2aa3b0b6d90e3
Dániel Czeglédi <132585056+czegi13@users.noreply.github.com>
2026-01-27 19:02:44 +0100

    valami fura volt

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './task.entity';
 
 @Module({
-  providers: [TasksService]
+  imports: [TypeOrmModule.forFeature([Task])],
+  providers: [TasksService],
+  controllers: [TasksController]
 })
 export class TasksModule {}

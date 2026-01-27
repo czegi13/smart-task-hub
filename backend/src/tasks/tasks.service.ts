e9ee50bb737d56fcf840d0c34eeba2ff3d5b3b15
Dániel Czeglédi <132585056+czegi13@users.noreply.github.com>
2026-01-27 18:58:17 +0100

    Tasks service constructor, findAll and create method

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -3,9 +3,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { Repository } from 'typeorm';
 
+
 @Injectable()
 export class TasksService {
-    constructor(@InjectRepository(Task) private repo: Repository<Task>){
-        
+    constructor(@InjectRepository(Task) private repo: Repository<Task>){   }
+
+    async findAll(): Promise<Task[]>{
+        return this.repo.find();
+    }
+
+    async create(title: string): Promise<Task>{
+        const newTask = this.repo.create({title});
+        return this.repo.save(newTask);
     }
 }

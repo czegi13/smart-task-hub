59deb66a1f220631d5f49bc0fb907c3baa37a0aa
Dániel Czeglédi <132585056+czegi13@users.noreply.github.com>
2026-01-27 19:38:15 +0100

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import api from './api';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const [tasks, setTasks] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchTasks = async() => {
+      const res = await api.get('/tasks');
+      setTasks(res.data);
+    };
+    fetchTasks();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: '20px'}}>
+      <h1>Smart-Task-Hub</h1>
+      {tasks.length === 0 ? (
+        <p>Nincsenek feladatok...</p>
+      ): (
+        <ul>
+          {tasks.map(t => <li key={t.id}>{t.title}</li>)}
+        </ul>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
